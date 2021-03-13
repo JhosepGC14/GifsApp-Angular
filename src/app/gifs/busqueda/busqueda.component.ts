d169940504acc207011772ed292422aabe97d871
Jhosep Guadalupe <jhosep.guadalupe>
2021-03-13 11:49:32 -0500

    implementando el viewChild para la busqueda

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
-  styleUrls: ['./busqueda.component.css']
+  styleUrls: ['./busqueda.component.css'],
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent {
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value;
+    console.log('valor : ', valor);
+    this.txtBuscar.nativeElement.value = '';
   }
-
 }
